6e9649b17d73942f0357363074a1600435cd4419
Noman Siddiqui <nomi.sid941@gmail.com>
2024-05-22 08:21:04 -0700

    initial commit

---
+++ olx/olx/settings.py
@@ -0,0 +1,116 @@
+# Scrapy settings for olx project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = "olx"
+
+SPIDER_MODULES = ["olx.spiders"]
+NEWSPIDER_MODULE = "olx.spiders"
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = "olx (+http://www.yourdomain.com)"
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+	
+# USER_AGENT: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)' # type: ignore
+
+# DOWNLOADER_MIDDLEWARES = {
+#     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+#     'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+#     'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+#     'scrapy_fake_useragent.middleware.RetryUserAgentMiddleware': 401,
+# }
+# USER_AGENT = '<your user agent string which you will fall back to if all other providers fail>'
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+#    "Accept-Language": "en",
+#}
+
+# Enable or disable spider middlewares
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    "olx.middlewares.OlxSpiderMiddleware": 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+# DOWNLOADER_MIDDLEWARES = {
+#    "olx.middlewares.OlxDownloaderMiddleware": 543,
+# #    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+# #     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+
+
+#     # 'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
+#     # 'scrapy.contrib.downloadermiddleware.retry.RetryMiddleware': None,
+#     # 'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+#     # 'scrapy_fake_useragent.middleware.RetryUserAgentMiddleware': 401,
+
+#     # 'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+#     # 'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+# }
+
+
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    "scrapy.extensions.telnet.TelnetConsole": None,
+#}
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    "olx.pipelines.OlxPipeline": 300,
+#}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = "httpcache"
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+
+# Set settings whose default value is deprecated to a future-proof value
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
---
+++ olx/olx/spiders/olx_mobiles.py
@@ -0,0 +1,40 @@
+import scrapy
+from urllib.parse import urlencode, urljoin
+import time
+import requests
+
+class OlxAddsSpider(scrapy.Spider):
+    name = "olx_mobiles"
+    allowed_domains = ["olx.com.pk"]
+    start_urls = ["https://www.olx.com.pk/mobile-phones_c1453"]
+ 
+    def parse(self, response):
+        # print(response)
+        hrefs = response.css("div.a52608cc > a::attr(href)").getall()
+        base_url = response.url  # Get the base URL
+        for href in hrefs:
+            
+            absolute_url = urljoin(base_url, href)
+            # time.sleep(0.5)
+            yield scrapy.Request(url=absolute_url, callback=self.parse_item)
+        yield from self.parse_nextpage(response)
+
+    def parse_item(self, response):
+        print(response.request.headers)
+        yield {
+            "Title":response.css("h1.a38b8112 ::text").get(),
+            "Price": response.css("span._56dab877::text").get(),
+            "Seller-Name":response.css("span._6d5b4928:nth-child(1).be13fe44 ::text").get(),
+            "Location":response.xpath("//div[@class='_1075545d' and .//span[@aria-label='Location']]//text()").get(),
+            "Brand":response.css("div._676a547f:nth-child(3) > div:nth-child(1) > span:nth-child(2) ::text").get(),
+            "Condition":response.css("div._676a547f:nth-child(4) > div:nth-child(1) > span:nth-child(2) ::text").get(),
+            "Is-Deliverable":response.css("div._676a547f > div:nth-child(1) > span:nth-child(2) ::text").get(),
+            "Description":response.css("._0f86855a>span ::text").get()
+            
+        }
+    def parse_nextpage(self, response):
+
+        for i in range(2, 50): 
+            pageurl = f"https://www.olx.com.pk/mobile-phones_c1453?page={i}"
+            # time.sleep(0.5)
+            yield scrapy.Request(url=pageurl, callback=self.parse)
